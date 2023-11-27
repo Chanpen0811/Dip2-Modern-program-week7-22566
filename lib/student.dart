bdfa6d05f95a6aad613df4bca36c4fba75fd77fe
chanpen0811 <jan1574p@gmail.com>
2023-11-27 15:08:51 +0700

    first commit

---
+++ lib/student.dart
@@ -0,0 +1,16 @@
+class Student{
+  //1.Properties
+  int? id;
+  String? name;
+  static String? schoolName = "Trattc";
+
+  //2.Constructor
+  Student(this. id,this.name);
+
+  //3.methods
+  void display(){
+    print(id);
+    print(name);
+    print(schoolName);
+  }
+}
